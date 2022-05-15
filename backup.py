d468b01b5ddc292e40145e2f2b3115e89efade72
csprock <csprock@gmail.com>
2022-05-14 20:38:46 -0600

    added output flag, fixed bug that archived entire cwd

--- backup.py
+++ backup.py
@@ -4,37 +4,49 @@ import shutil
 import logging
 from pathlib import Path
 
-logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(name=__name__)
+logger.setLevel(level=logging.DEBUG)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
 
 parser = argparse.ArgumentParser(description="Compress contents of a directory")
 parser.add_argument("--input")
+parser.add_argument("--output", default=None)
 parser.add_argument("--exclude_hidden", action="store_true", default=False)
+parser.add_argument("--dry-run", action="store_true", default=False)
 
 args = parser.parse_args()
 
 def list_directories(path):
     if args.exclude_hidden:
+        logger.info("Excluding hidden files")
         return [Path(x) for x in Path(path).iterdir() if x.is_dir() and not x.match(".*")]
     else: 
         return [Path(x) for x in Path(path).iterdir() if x.is_dir()]
 
-
+logger.info(f"Dry run: {args.dry_run}")
 
 def main(input, output=None):
     input = Path(input).expanduser()
     directories = list_directories(input)
 
+    if args.output:
+        base_name = args.output
+    else:
+        base_name = args.input
+
     for dir in directories:
-        logging.debug(dir)
+        logger.info(dir)
 
         if not dir.match(".*"):
             shutil.make_archive(
-                base_name = dir.name,
+                base_name = base_name,
+                root_dir = dir.name,
                 format='gztar',
-                dry_run = True
+                dry_run = args.dry_run,
+                logger=logger
             )
-
-    return directories
     
 
 if __name__ == "__main__":
