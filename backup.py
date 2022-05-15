3b1f6e99c9e7c5c12a712932e1299abf56d73ba3
csprock <csprock@gmail.com>
2022-05-14 21:38:44 -0600

    added output flag and move command. Now moves newly created archives into the directory specified in --output

--- backup.py
+++ backup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import argparse
+from re import L
 import shutil
 import logging
 from pathlib import Path
 
 
-logger = logging.getLogger(name=__name__)
+logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 handler = logging.StreamHandler()
 logger.addHandler(handler)
 
 parser = argparse.ArgumentParser(description="Compress contents of a directory")
 parser.add_argument("--input")
-parser.add_argument("--output", default=None)
+parser.add_argument("--output")
 parser.add_argument("--exclude_hidden", action="store_true", default=False)
 parser.add_argument("--dry-run", action="store_true", default=False)
 
@@ -27,27 +28,36 @@ def list_directories(path):
 
 logger.info(f"Dry run: {args.dry_run}")
 
-def main(input, output=None):
-    input = Path(input).expanduser()
+
+def main(input, output):
+    input = Path(input).resolve()
+    output = Path(output).resolve()
+
+    if not output.is_dir():
+        raise FileExistsError(f"{str(output)} is not a directory.")
+
     directories = list_directories(input)
 
-    if args.output:
-        base_name = args.output
-    else:
-        base_name = args.input
+    try:
+        directories.remove(output)
+        logger.info("Destination path found in compression target, skipping")
+    except ValueError:
+        pass
 
     for dir in directories:
         logger.info(dir)
 
         if not dir.match(".*"):
             shutil.make_archive(
-                base_name = base_name,
+                base_name = dir.name,
                 root_dir = dir.name,
                 format='gztar',
                 dry_run = args.dry_run,
                 logger=logger
             )
+            if Path(dir.name + ".tar.gz").exists():
+                shutil.move(src=dir.name + ".tar.gz", dst=args.output)
     
 
 if __name__ == "__main__":
-    main(args.input)
+    main(args.input, args.output)
