9b5452d2ebff608ebe69f332e6bc94c0e6ff3e60
csprock <csprock@gmail.com>
2022-05-14 20:22:29 -0600

    added exclude_hidden flag and dry run compression

--- backup.py
+++ backup.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 import argparse
+import shutil
+import logging
 from pathlib import Path
+
+logging.basicConfig(level=logging.DEBUG)
 
 parser = argparse.ArgumentParser(description="Compress contents of a directory")
 parser.add_argument("--input")
+parser.add_argument("--exclude_hidden", action="store_true", default=False)
 
 args = parser.parse_args()
 
 def list_directories(path):
-    return [Path(x) for x in Path(path).iterdir() if Path.is_dir(x)]
+    if args.exclude_hidden:
+        return [Path(x) for x in Path(path).iterdir() if x.is_dir() and not x.match(".*")]
+    else: 
+        return [Path(x) for x in Path(path).iterdir() if x.is_dir()]
+
 
 
 def main(input, output=None):
+    input = Path(input).expanduser()
     directories = list_directories(input)
+
+    for dir in directories:
+        logging.debug(dir)
+
+        if not dir.match(".*"):
+            shutil.make_archive(
+                base_name = dir.name,
+                format='gztar',
+                dry_run = True
+            )
+
     return directories
     
 
 if __name__ == "__main__":
-    print(main(args.input))
+    main(args.input)
