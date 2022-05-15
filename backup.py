7e836a87bbb736efe5cbebe01f0d9ddb7fd01134
csprock <csprock@gmail.com>
2022-05-14 19:57:37 -0600

    added list_directories

--- backup.py
+++ backup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description="Compress contents of a directory")
+parser.add_argument("--input")
+
+args = parser.parse_args()
+
+def list_directories(path):
+    return [Path(x) for x in Path(path).iterdir() if Path.is_dir(x)]
+
+
+def main(input, output=None):
+    directories = list_directories(input)
+    return directories
+    
 
 if __name__ == "__main__":
-    pass
+    print(main(args.input))
