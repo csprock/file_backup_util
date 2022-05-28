fd9e045313994d8e245e3479e839ae9c37341adf
csprock <csprock@gmail.com>
2022-05-28 16:54:51 -0600

    added yaml integration, works

--- backup.py
+++ backup.py
@@ -6,7 +6,7 @@ import shutil
 import logging
 from pathlib import Path
 from datetime import datetime
-from configparser import ConfigParser
+import yaml
 
 EXTENTION_MAP = {
     'gztar':'.tar.gz',
@@ -20,48 +20,41 @@ LOGGER.addHandler(handler)
 
 parser = argparse.ArgumentParser(description="Compress contents of a directory")
 parser.add_argument("--config")
+parser.add_argument("--mode", default=511)
 
 args = parser.parse_args()
 
-config = ConfigParser()
-config.read(args.config)
+with open(args.config, mode='rt') as file:
+    config = yaml.safe_load(file)
+
 
 src_root = Path(args.config).parent
 
 
-def _bool(value):
-    if value == 'True':
-        return True
-    if value == 'False':
-        return False
-    else:
-        raise TypeError(f"value must be str True or False, found {value} instead")
-        
-try:
-    config['Options']['suffix']
-except KeyError:
-    config['Options']['suffix'] = default=datetime.now().isoformat()
+if config['options']['suffix'] is None:
+    config['options']['suffix'] = datetime.now().isoformat().replace(":", "-")
 
-LOGGER.info(f"Dry run: {(config['Options']['dry_run'])}")
+
+LOGGER.info(f"Dry run: {(config['options']['dry_run'])}")
 
 def main():
 
-    dest_dir = Path(config['Options']['destination'] + "_" + config['Options']['suffix']).resolve()
-    dest_dir.mkdir(parents=True, exist_ok=True)
+    dest_dir = Path(config['options']['destination'] + "_" + config['options']['suffix']).resolve()
+    dest_dir.mkdir(mode=args.mode, parents=True, exist_ok=True)
     LOGGER.info(f"Making directory {dest_dir}")
 
 
-    for file in config['Files']:
+    for file, params in config['files'].items():
 
-        format = config['Files'][file]
+        format = params['format']
         src_name = src_root / Path(file)
         LOGGER.info(f"Backing up {src_name}")
 
-        if format == 'None':
+        if format is None:
             pass
             dest_name = dest_dir / Path(file)
             LOGGER.info(f"Copying {src_name} without compression to {dest_name}")
-            if not _bool(config['Options']['dry_run']):
+            if not config['options']['dry_run']:
                 d = shutil.copytree(str(src_name), str(dest_name), dirs_exist_ok=True)
                 LOGGER.info(d)
         else:
@@ -72,10 +65,10 @@ def main():
                 base_name = str(src_name),
                 root_dir = str(src_name),
                 format=format,
-                dry_run = _bool(config['Options']['dry_run']),
+                dry_run = config['options']['dry_run'],
                 logger=LOGGER
             )
-            if not _bool(config['Options']['dry_run']):
+            if not config['options']['dry_run']:
                 archive_name = str(src_name) + EXTENTION_MAP[format]
                 LOGGER.info(f"Attempting to move {archive_name} to {dest_dir}")
                 dest = shutil.move(src=archive_name, dst=str(dest_dir))
